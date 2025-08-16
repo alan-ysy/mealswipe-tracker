c29c5da196b8ce9405e3e90d69b76d0110b1a7fa
Alan Yeung <184780131+arlainn@users.noreply.github.com>
2025-08-17 00:08:51 +0800

    Set up objects for meal swipe limits

---
+++ script.js
@@ -0,0 +1,29 @@
+const mealPlanWeeklySwipes = {
+    "universal": {
+        weekly: 0,
+        premium: 0,
+        grabAndGo: 0,
+        lateNight: 0,
+        convenience: 0,
+        portable: 0,
+        scholar: 0
+    },
+    "17-classic": {
+        weekly: 0,
+        premium: 0,
+        grabAndGo: 0,
+        lateNight: 0,
+        convenience: 0,
+        portable: 0,
+        scholar: 0
+    },
+    "14-halal-kosher": {
+        weekly: 0,
+        premium: 0,
+        grabAndGo: 0,
+        lateNight: 0,
+        convenience: 0,
+        portable: 0,
+        scholar: 0
+    }
+}
